e6804b3001280f98cbb491663bf3fec05d200c98
Torrey Hogan <hogantorrey@gmail.com>
2018-10-26 14:30:09 -0700

    fully functional game

--- mastermind.rb
+++ mastermind.rb
@@ -4,18 +4,11 @@ end
 class Board
 
 	attr_accessor :board, :turns
-
+	SOLSOL = ["4", "5", "6", "7"]
 	def initialize(number_turns)
 		@turns = number_turns
-		@solution = ["1", "2", "3", "4" ]#[ rand(7).to_s , rand(7).to_s , rand(7).to_s , rand(7).to_s ]
+		@solution = [ rand(7).to_s , rand(7).to_s , rand(7).to_s , rand(7).to_s ]
 	end
-
-	# def random_color
-	# 	color = { 1 => "blue", 2 => "green", 3 => "orange", 
-	# 						4 => "purple", 5 => "red", 6 => "yellow"}
-	# 	random_solution = [ color[rand(7)] , color[rand(7)] , color[rand(7)] , color[rand(7)] ]
-	# 	return random_solution
-	# end
 		
 	def board_setup
 		@board = {}
@@ -27,7 +20,7 @@ class Board
 	end
 
 	def display_board
-		@board.each { |guess_number, value|
+		@board.each do |guess_number, value|
 			string = "- #{}"#{guess_number} --> "
 			value["guess"].each { |item|
 				string += "#{item} "
@@ -37,7 +30,7 @@ class Board
 				string += "#{item} "
 			}
 			puts string
-		}
+		end
 	end
 
 	def guess(turn_number)
@@ -52,16 +45,15 @@ class Board
 		current_turn = 1
 		display_board
 		guess(current_turn)
-		solu = @solution
-		feedback(current_turn, solu)
-		while current_turn < 12 && solu != @board[current_turn]["guess"] #!winner(current_turn)
+		feedback(current_turn)
+		while current_turn < 12 && @solution != @board[current_turn]["guess"] #!winner(current_turn)
 			current_turn += 1
 			display_board
 			guess(current_turn)
-			feedback(current_turn, solu)
+			feedback(current_turn)
 		end
 
-		if @board[current_turn]["guess"] == solu
+		if @board[current_turn]["guess"] == @solution
 			puts "Congratulations!!! You stopped the nuclear detonation!!"
 		else
 			puts "The bomb exploded!!!!"
@@ -69,40 +61,38 @@ class Board
 		end
 	end
 
-	def feedback(current_turn, solution_arr_in)
+	def feedback(current_turn)
 		# Tests for matching numbers and index, returns "b"
-		@board[current_turn]["guess"].each_with_index { |el, idx|
+		solution_arr = @solution.dup  # SOLSOL this change fixed it
+		@board[current_turn]["guess"].each_with_index do |el, idx|
 			spot_open = feedback_el(current_turn)
-			if solution_arr_in[idx] == el && spot_open != false
+			if @solution[idx] == el && spot_open != false
 				@board[current_turn]["feedback"][spot_open] = "b"
 			end
-		}
+		end
 
 		# tests for if the the number is in the solution, returns "w"
-		solution_arr = solution_arr_in
-		@board[current_turn]["guess"].each_with_index { |el, idx|
+		@board[current_turn]["guess"].each_with_index do |el, idx|
 			spot_open = feedback_el(current_turn)
-				if solution_arr.include?(el) && solution_arr_in[idx] != el && spot_open != false 
+				if solution_arr.include?(el) && @solution[idx] != el && spot_open != false 
 					@board[current_turn]["feedback"][spot_open] = "w"
 					solution_arr.delete_at(solution_arr.index(el))
 				end
-			}
-			print @solution
+			end
 
 	end
 
 	def feedback_el(current_turn)
-		@board[current_turn]["feedback"].each_with_index { |el, idx|
+		@board[current_turn]["feedback"].each_with_index do |el, idx|
 			if el == "-"
 				return idx
 			end
-		}
+		end
 		false
 	end
 end
 
 new_board = Board.new(12)
-
 new_board.play
 
 
